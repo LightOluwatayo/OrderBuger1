7e3ebbf8804e180e07de2a23a25a7d33b842bccc
LIGHT BLESSING FOREVER <blessbamz@gmail.com>
2022-06-13 10:50:36 -0500

    Add project files.

---
+++ OrderBuger1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="OrderBuger1.MvcApplication" Language="VB" %>
---
+++ OrderBuger1/Models/Account.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Login.Models
+{
+
+    Public class Account
+    { 
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+    
+    
+    }
+
+}
+
+
+
